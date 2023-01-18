bbe057618a8c834552cf27aa28c5068a3e39846a
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-01-18 13:06:55 +0600

    last commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,106 @@
-import React, { useRef, useState } from "react";
-import { Route, Routes } from "react-router-dom";
-import Favourites from "./components/Favourites";
-import Footer from "./components/Footer";
+import { useEffect, useRef, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Favourites from "./components/Favourites";
 import NotFound from "./components/NotFound";
+import RecipeItem from "./components/RecipeItem";
 
 const App = () => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [savedItems, setSavedItems] = useState(() => {
+    const localData = localStorage.getItem("recipes");
+    return localData ? JSON.parse(localData) : [];
+  });
 
   const inputField = useRef(null);
 
-  const searchHandeler = (e) => {
-    e.preventDefault();
+  const navigate = useNavigate();
 
-    console.log(searchQuery);
+  const searchHandler = (e) => {
+    e.preventDefault();
 
     getData(searchQuery);
 
     setSearchQuery("");
     inputField.current.blur();
+    setRecipes([]);
+    setError("");
+    navigate("/");
   };
+
   const getData = async (searchQuery) => {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes?search=/${searchQuery}`
-    );
-    const data = await res.json();
-    console.log(data);
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchQuery}`
+      );
+      if (!res.ok) throw new Error("Something went wrong!");
+      const data = await res.json();
+      if (data.results === 0) throw new Error("No recipe found!");
+      setRecipes(data?.data?.recipes);
+      setLoading(false);
+    } catch (err) {
+      setError(err.message);
+    }
   };
+
+  const checkLocalData = (data) => {
+    const localData = JSON.parse(localStorage.getItem("recipes"));
+    const existedData = localData?.some((item) => item.id === data.id);
+
+    if (!existedData) {
+      setSavedItems([...savedItems, data]);
+    } else {
+      const filteredData = localData.filter((item) => item.id !== data.id);
+      setSavedItems(filteredData);
+    }
+  };
+
+  const favouriteHandler = (id) => {
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
+      .then((res) => res.json())
+      .then((data) => checkLocalData(data.data.recipe));
+
+    navigate("/favourites");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(savedItems));
+  }, [savedItems]);
+
   return (
     <>
       <div className="app min-h-screen bg-rose-50 text-gray-600 text-lg">
         <Navbar
+          savedItems={savedItems}
           searchQuery={searchQuery}
           setSearchQuery={setSearchQuery}
           inputField={inputField}
-          searchHandler={(e) => searchHandeler}
+          searchHandler={searchHandler}
         />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favprotes" element={<Favourites />} />
+          <Route
+            path="/"
+            element={<Home recipes={recipes} loading={loading} error={error} />}
+          />
+          <Route
+            path="/favourites"
+            element={<Favourites savedItems={savedItems} />}
+          />
+          <Route
+            path="/recipe-item/:id"
+            element={
+              <RecipeItem
+                favouriteHandler={favouriteHandler}
+                savedItems={savedItems}
+              />
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,41 @@
 import { NavLink } from "react-router-dom";
 
 const Navbar = ({
-  searchHandeler,
+  searchHandler,
   searchQuery,
   setSearchQuery,
   inputField,
+  savedItems,
 }) => {
   const navActive = ({ isActive }) => {
     return {
       color: isActive ? "#f43f5e" : null,
     };
   };
+
   return (
-    <div
-      className="navbar flex justify-between items-center container mx-auto py-8 flex-col lg:flex-row gap-5 lg:gap-0 
-    "
-    >
+    <div className="navbar flex justify-between items-center container mx-auto py-8 flex-col lg:flex-row gap-5 lg:gap-0">
       <h2 className="logo text-2xl font-bold lowercase italic">
-        food<span className="text-rose-500">verse</span>
+        Food<span className="text-rose-500">verse</span>
       </h2>
-      <form className="search-bar" onSubmit={searchHandeler}>
+      <form className="search-bar" onSubmit={searchHandler}>
         <input
           ref={inputField}
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           type="search"
-          placeholder="search something"
+          placeholder="Search recipe..."
           required
           className="bg-white/75 p-3 px-8 lg:w-96 rounded-full outline-none shadow-lg shadow-rose-100 focus:shadow-rose-200 duration-300"
         />
       </form>
-      <ul className="manu flex gap-5">
+      <ul className="menu flex gap-5">
         <li>
           <NavLink
             style={navActive}
             end
             to="/"
-            className="text-gray-400 hover:text-gray-600"
+            className="text-gray-400 hover:text-gray-600 duration-300"
           >
             Home
           </NavLink>
@@ -44,11 +43,13 @@ const Navbar = ({
         <li>
           <NavLink
             style={navActive}
-            to="/favorites"
-            className="text-gray-400 hover:text-gray-600"
+            to="/favourites"
+            className="text-gray-400 hover:text-gray-600 duration-300"
           >
-            Favorites{""}
-            <span className="favorites-count text-sky-400 font-bold">(10)</span>
+            Favourites{" "}
+            <span className="favourites-count font-bold text-sky-400">
+              ({savedItems.length})
+            </span>
           </NavLink>
         </li>
       </ul>
